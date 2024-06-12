62d587031c53fc3edd6702cbcc950d54af42e5ae
Anirban Banai <anirbanchondrobanai@gmail.com>
2024-06-12 09:54:46 +0600

    section 1 added

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,10 +1,9 @@
-import { Button } from '@material-tailwind/react';
+import Section1 from "./Section1";
 
 const Home = () => {
   return (
     <div>
-      <h1>This is home page</h1>
-      <Button>Material Tailwind Button</Button>
+      <Section1 />
     </div>
   );
 };

66880baf89056f0c53b3417e6cc1aaa67d9d775c
Kutub Udd <socialnetwork98710@gmail.com>
2024-06-19 17:22:35 +0600

    added

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   },
   server: {
-    port: 3000,
+    port: 5173,
   },
 });

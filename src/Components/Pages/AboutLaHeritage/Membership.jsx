f15036ff1b7b004d9bb8034ba90686234cb0bfcf
Kutub Udd <socialnetwork98710@gmail.com>
2024-07-14 10:50:32 +0600

    added

--- src/Components/Pages/AboutLaHeritage/Membership.jsx
+++ src/Components/Pages/AboutLaHeritage/Membership.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MembershipHome = () => {
   return (
     <div>
-      <h1>MembershipHome</h1>
+      <h1>https://meet.google.com/yxg-qzxk-ixr</h1>
     </div>
   );
 };
